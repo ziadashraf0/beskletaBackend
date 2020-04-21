afe225229936747e9f2210b6a9005ad63801af45
ziadashraf0 <ziadshreet@gmail.com>
2020-04-21 04:28:59 +0200

    server

--- index.js
+++ index.js
@@ -20,22 +20,35 @@ app.use(BodyParser.urlencoded({ extended: true }));
 var url =
   "mongodb://<heroku_x8h0zwnq>:<beskletab1>@ds361085.mlab.com:61085/heroku_x8h0zwnq";
 //Connecting to Beskleta DataBase ;
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  })
-  .then(
-    () => {
-      /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
-      console.log("DATABASE Created !");
-    },
-    err => {
-      /** handle initial connection error */
-      console.error("Could not connect to mongoDB");
+mongoose.connect(
+  "mongodb://heroku_x8h0zwnq:beskletab1@ds361085.mlab.com:61085/heroku_x8h0zwnq",
+  { useNewUrlParser: true },
+  function(err) {
+    {
+      if (err) {
+        console.log("Some problem with the connection " + err);
+      } else {
+        console.log("The Mongoose connection is ready");
+      }
     }
-  );
+  }
+);
+// mongoose
+//   .connect(url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true
+//   })
+//   .then(
+//     () => {
+//       /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
+//       console.log("DATABASE Created !");
+//     },
+//     err => {
+//       /** handle initial connection error */
+//       console.error(err);
+//     }
+//   );
 app.use("/bike", Bike);
 app.use("/owner", Owner);
 app.use("/admin", Admin);
